ad1d91e60e53e738cd318239cfd2a944c4c76206
Matheus Marchini <matheus@sthima.com.br>
2017-09-26 15:34:51 -0300

    src: refactoring of sdtnotes and string tables
    Refactoring step 1: created auxiliar structures and functions to
    String Tables (shstrtab and dynsym) and replaced static declaration of
    strings to a more dynamic one (still needs some refactor here though).
    Also moved SDTNote structure and functions to a separate file.

---
+++ src/string-table.h
@@ -0,0 +1,26 @@
+#ifndef _STRING_TABLE_H
+#define _STRING_TABLE_H
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct StringTableNode_ {
+  int index;
+  int size;
+  char *str;
+  struct StringTableNode_ *next;
+} StringTableNode;
+
+typedef struct {
+  int count;
+  size_t size;
+  StringTableNode *first;
+} StringTable;
+
+StringTable *stringTableInit();
+
+StringTableNode *stringTableAdd(StringTable *stringTable, char *str);
+
+char *stringTableToBuffer(StringTable *stringTable);
+
+#endif
