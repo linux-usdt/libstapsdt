a896827e0e09c5a654804534bfb1dc9b6ae804df
Mary Marchini <oss@mmarchini.me>
2017-09-27 11:06:53 -0300

    src: sections refactor
    Instead of using multiple variables to hold information regarding each
    section, we now use a struct which holds everything needed to create and
    manipulate sections data and metadata. Code should be more readable and
    maintainable now.

---
+++ src/section.h
@@ -0,0 +1,17 @@
+#ifndef _SECTION_H
+#define _SECTION_H
+
+#include <libelf.h>
+
+#include "string-table.h"
+
+typedef struct {
+  Elf_Scn *scn;
+  Elf64_Shdr *shdr;
+  Elf_Data *data;
+  Elf64_Addr offset;
+
+  StringTableNode *string;
+} Section;
+
+#endif
