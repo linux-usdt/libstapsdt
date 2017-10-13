69fa47f33634a7527351d22eb63e891a617ac80b
Mary Marchini <oss@mmarchini.me>
2017-10-13 10:16:01 -0300

    src: fix hashtable algorithm (#12)
    Hashtable algorithm was wrongly implemented, leading to problems when
    we have hash collisions. This new implementation follows the
    specifications defined below.
    
    Ref: http://www.sco.com/developers/gabi/latest/ch5.dynamic.html#hash
    Fixex: https://github.com/sthima/libstapsdt/issues/11

--- src/hash-table.c
+++ src/hash-table.c
@@ -1,41 +1,25 @@
 #include <stdio.h>
 #include "dynamic-symbols.h"
 
-#define possibleBucketSizesCount 17
-size_t possibleBucketSizes[] = {1, 3, 7, 13, 31, 61, 127, 251, 509, 1021, 2039, 4093, 8191, 16381, 32749, 65521, 131071};
-
-static size_t getRecommendedBucketSize(size_t NumSymbols) {
-  // Fallback
-  size_t bucketSize = NumSymbols * 2;
-
-  for (int i=0; i < possibleBucketSizesCount; i++) {
-    if (NumSymbols < possibleBucketSizes[i]) {
-      bucketSize = possibleBucketSizes[i];
-      break;
-    }
-  }
-
-  return bucketSize;
-}
-
-// TODO (mmarchini) maybe we should use GNU Hash instead
 size_t hashTableFromSymbolTable(DynamicSymbolTable *table, uint32_t **hashTable) {
   DynamicSymbolList *current;
-  uint32_t nBuckets = getRecommendedBucketSize(table->count),
-           nChains = table->count + 1;
+  uint32_t nBuckets = table->count + 3,  // + 3, to count bss, end, and eData symbols
+           nChains = table->count + 3;
   size_t hashTableSize = (nBuckets + nChains + 2);
 
   uint32_t *hashTable_ = (uint32_t *)calloc(sizeof(uint32_t), hashTableSize);
-  int idx, chainIdx=1;
+  uint32_t *buckets = &(hashTable_[2]);
+  uint32_t *chains = &(buckets[nBuckets]);
+  int idx, stringIdx=2;
 
   hashTable_[0] = nBuckets;
   hashTable_[1] = nChains;
 
   for(current=table->symbols; current!=NULL; current=current->next) {
     idx = elf_hash(current->symbol.string->str) % nBuckets;
-    hashTable_[2 + idx] = chainIdx;
-    hashTable_[2 + nBuckets + chainIdx] = chainIdx + 1;
-    chainIdx++;
+    chains[stringIdx] = buckets[idx];
+    buckets[idx] = stringIdx;
+    stringIdx++;
   }
 
   *hashTable = hashTable_;
